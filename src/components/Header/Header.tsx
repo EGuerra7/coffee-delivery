8d5ce37b757b83ad504d97401f433210124dec89
Erick Guerra <erickmuzzi@gmail.com>
2024-12-10 10:15:55 -0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,24 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer, PlaceItem, ShopButton } from "./styles";
 import logoCoffee from '../../assets/coffe-logo.svg';
-import { MapPin } from "phosphor-react";
+
+import { useTheme } from "styled-components";
+import { MapPin, ShoppingCart } from "phosphor-react";
 
 export default function Header() {
+  const theme = useTheme();
+
   return (
     <HeaderContainer>
         <img src={logoCoffee} />
 
         <section>
-            <div>
-                <MapPin size={22}/>
+            <PlaceItem>
+                <MapPin size={22} color={theme.purple} weight="fill"/>
                 <span>Porto Alegre, RS</span>
-            </div>
+            </PlaceItem>
+            <ShopButton>
+              <ShoppingCart size={22} weight="fill"/>
+            </ShopButton>
         </section>
     </HeaderContainer>
     
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,4 +6,31 @@ export const HeaderContainer = styled.header`
     align-items: center;
     justify-content: space-between;
     padding: 2rem 0;
+
+    section{
+        display: flex;
+        gap: 0.75rem;
+        align-items: center;
+    }
+`
+export const PlaceItem = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    background: ${props => props.theme["purple-light"]};
+    color: ${props => props.theme["purple-dark"]};
+    padding: 0.5rem;
+    border-radius: 8px;
+    font-size: 0.875rem;
+`
+
+export const ShopButton = styled.button`
+    background-color: ${props => props.theme["yellow-light"]};
+    color: ${props => props.theme["yellow-dark"]};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 8px;
+    border-radius: 8px;
+    border: 0;
 `
